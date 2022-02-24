e0309e1b11e3a0e6a5c9baf243cc07ad54d88fc9
zombie110year <zombie110year@outlook.com>
2022-02-25 00:08:51 +0800

    func(brainfuck): read file to execute

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod bfvm;
 
-const CODE: &'static str = include_str!("../bf_scripts/helloworld.bf");
-
 fn main() {
-    let mut vm = bfvm::BFVM::new();
-    vm.run(CODE.into());
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 2 {
+        let script_file = &args[1];
+        let code = std::fs::read_to_string(script_file).unwrap();
+        let mut vm = bfvm::BFVM::new();
+        vm.run(code.into());
 
-    #[cfg(debug_assertions)]
-    println!("{:?}", vm);
+        // debug 模式编译时 debug_assertions 被启用, release 时则没有
+        #[cfg(debug_assertions)]
+        println!("{:?}", vm);
+    } else {
+        println!("Usage: {} <script.bf>", args[0])
+    }
 }
