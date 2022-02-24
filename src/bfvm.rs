cb5f9d744aa4d00333ed02b85af9b24c32455222
zombie110year <zombie110year@outlook.com>
2022-02-25 00:01:02 +0800

    func(bfvm): jump

--- src/bfvm.rs
+++ src/bfvm.rs
@@ -1,5 +1,6 @@
 //! BrainFuck Virtual Machine
 
+use std::collections::HashMap;
 use std::io::{Read, Write};
 
 /// # BrainFuck Virtual Machine
@@ -9,16 +10,29 @@ pub struct BFVM {
     mem: Vec<u8>,
     /// 用于记录指针位置
     p: usize,
+    /// 用于标记指令执行位置
+    codep: usize,
+    /// 跳转标记
+    jump_sign: HashMap<usize, usize>,
 }
 
 // 对外 API
 impl BFVM {
     pub fn new() -> Self {
-        Self { mem: vec![0], p: 0 }
+        Self {
+            mem: vec![0],
+            p: 0,
+            codep: 0,
+            jump_sign: HashMap::new(),
+        }
     }
 
     pub fn run(&mut self, code: String) {
-        for op in code.chars() {
+        let code: Vec<char> = code.chars().collect();
+        let codel = code.len();
+        self.scan_jump(&code);
+        while self.codep < codel {
+            let op = code[self.codep];
             match op {
                 '>' => self.op_right(),
                 '<' => self.op_left(),
@@ -30,12 +44,38 @@ impl BFVM {
                 ']' => self.op_jumpback(),
                 _ => (),
             }
+            self.codep += 1;
         }
     }
 }
 
 // 各操作的实现
 impl BFVM {
+    fn scan_jump(&mut self, code: &Vec<char>) {
+        let mut stack = Vec::new();
+        for (i, c) in code.iter().enumerate() {
+            match c {
+                '[' => stack.push(i),
+                ']' => {
+                    if let Some(j) = stack.pop() {
+                        self.jump_sign.insert(j, i);
+                        self.jump_sign.insert(i, j);
+                    } else {
+                        panic!(
+                            "第 {} 个字符（指令 '{}'）：在前方缺少跳转标记 '['",
+                            i + 1,
+                            c
+                        );
+                    }
+                }
+                _ => (),
+            }
+        }
+        if !stack.is_empty() {
+            panic!("存在未闭合的跳转指令 '['");
+        }
+    }
+
     fn op_right(&mut self) {
         self.p += 1;
         // 由于 index 和 length 相差 1，因此这里在相等时就需要增长一位
@@ -49,11 +89,11 @@ impl BFVM {
     }
 
     fn op_add(&mut self) {
-        self.mem[self.p] += 1;
+        self.mem[self.p] = self.mem[self.p].wrapping_add(1);
     }
 
     fn op_sub(&mut self) {
-        self.mem[self.p] -= 1;
+        self.mem[self.p] = self.mem[self.p].wrapping_sub(1);
     }
 
     fn op_out(&self) {
@@ -71,11 +111,19 @@ impl BFVM {
         std::io::stdin().lock().read(&mut buf).unwrap();
     }
 
-    fn op_jump(&self) {
-        todo!()
+    fn op_jump(&mut self) {
+        if self.mem[self.p] == 0 {
+            if let Some(jumpto) = self.jump_sign.get(&self.codep) {
+                self.codep = *jumpto;
+            }
+        }
     }
 
-    fn op_jumpback(&self) {
-        todo!()
+    fn op_jumpback(&mut self) {
+        if self.mem[self.p] != 0 {
+            if let Some(jumpto) = self.jump_sign.get(&self.codep) {
+                self.codep = *jumpto;
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod bfvm;
 
+const CODE: &'static str = include_str!("../bf_scripts/helloworld.bf");
 
 fn main() {
     let mut vm = bfvm::BFVM::new();
-    vm.run(",>,>,<<+.>+.>+.".into());
+    vm.run(CODE.into());
+
+    #[cfg(debug_assertions)]
     println!("{:?}", vm);
 }
